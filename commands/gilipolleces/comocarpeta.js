5ecd62e67e39dcf1ddcf49efdb4e490ac6dcb29a
amna <antoniomiguel2001@hotmail.es>
2023-10-31 11:23:30 +0100

    Update comocarpeta.js

--- commands/gilipolleces/comocarpeta.js
+++ commands/gilipolleces/comocarpeta.js
@@ -4,7 +4,7 @@ module.exports = {
     cooldown: 10,
     data: new SlashCommandBuilder()
         .setName('carpet')
-        .setDescription('no tiene nada que ver pero ruben juega persona 5 o te meto 200 horas de mute mas las pajas que me he hecho en toda mi vida es decir te mueres antes de ser desmuteado de este discord'),
+        .setDescription('ruben juega persona 5 o te meto 200 horas de mute mas las pajas que me he hecho en mi vida'),
     async execute(interaction) {
         await interaction.reply({
             content: `CHECK IT OUT 💥 👊 IM IN THE HOUSE 🏠 :interrobang: LIKE CARPET :skull_crossbones: :bangbang:`,
