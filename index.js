cfd926b2d2e09e96dc48a53488b8d3e0925ea201
amna <antoniomiguel2001@hotmail.es>
2024-09-23 19:18:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -127,9 +127,17 @@ client.on('messageCreate', (message) => {
         const random = Math.random();
         if (random < 0.05) {
             console.log(random);
-            message.reply('https://cdn.discordapp.com/attachments/1247185053017178162/1278703542793670738/Ae5060WhLO399Y_k.mp4?ex=66d5b979&is=66d467f9&hm=c004d5a96c0dd289c366f077072d49a5ba3fbcbdbc22d3d21bf9f1e72b2e6bf0&');
+            message.reply('[TOMAAAAAAAAAAAAA](https://cdn.discordapp.com/attachments/1247185053017178162/1278703542793670738/Ae5060WhLO399Y_k.mp4?ex=66d5b979&is=66d467f9&hm=c004d5a96c0dd289c366f077072d49a5ba3fbcbdbc22d3d21bf9f1e72b2e6bf0&)');
         }
     }
+});
+
+client.on('messageCreate', (message) => {
+	const random = Math.random();
+	if (random < 0.001) {
+	  console.log(random)
+	  message.reply('[esto deberia ser para alva pero no esta aqi](https://cdn.discordapp.com/attachments/1247185053017178162/1278703542793670738/Ae5060WhLO399Y_k.mp4?ex=66d5b979&is=66d467f9&hm=c004d5a96c0dd289c366f077072d49a5ba3fbcbdbc22d3d21bf9f1e72b2e6bf0&)');
+	}
 });
 
 const nombres = ['alva', 'ernesto', 'alex', 'maicro', 'ruben', 'lucia', 'manuel'];
